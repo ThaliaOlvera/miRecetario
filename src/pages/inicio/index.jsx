17a1948a247bcf69fa829c3172dd14208a72ced0
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-03 17:20:43 -0500

    Add: Titulo component

--- src/pages/inicio/index.jsx
+++ src/pages/inicio/index.jsx
@@ -1,11 +1,15 @@
 import Header from "componentes/Header/Header.jsx";
 import Banner from "../../componentes/Banner";
+import Titulo from "../../componentes/Titulo";
 
 function Inicio() {
   return (
     <>
       <Header />
       <Banner img="home" color="#154580" />
+      <Titulo>
+        <h1>Un lugar para guardar tus recetas favoritas</h1>
+      </Titulo>
     </>
   );
 }
