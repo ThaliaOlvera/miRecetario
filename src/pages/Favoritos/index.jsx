fd58b5de1856f215744c954852d223031e8d5ec1
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-07 16:35:15 -0500

    Add: Favoritos Context

--- src/pages/Favoritos/index.jsx
+++ src/pages/Favoritos/index.jsx
@@ -1,10 +1,23 @@
 import Banner from "../../componentes/Banner";
+import Titulo from "../../componentes/Titulo";
 import styles from "./Favoritos.module.css";
+import { useFavoritosContext } from "../../context/Favoritos";
+import Card from "../../componentes/Card";
 
 function Favoritos() {
+  const { favorito } = useFavoritosContext();
+
   return (
     <>
       <Banner img="home" color="#154580" />
+      <Titulo>
+        <h1>Mis Favortitos</h1>
+      </Titulo>
+      <section className={styles.container}>
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </section>
     </>
   );
 }
