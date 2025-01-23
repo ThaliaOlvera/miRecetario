800513480dacb42d43a3d7feaff9b154472903d2
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-23 12:44:27 -0500

    Deploy 1

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "/miRecetario/index.css";
+import "/index.css";
 
 import AppRoutes from "./routes";
 import { VideoProvider } from "./context/VideosContext";
