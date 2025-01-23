0e500c134d2e11e264cee6746d0a4381f865e32e
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-23 12:07:12 -0500

    Fix: Build

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import path from "path"; // Importar el módulo path
 export default defineConfig({
   plugins: [react()],
   base: "/miRecetario/",
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"), // Alias para la carpeta 'src'
-      componentes: path.resolve(__dirname, "src/componentes"), // Alias específico para componentes
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
     },
   },
 });
