b6bddb8faddd21d16b33568d7514f55010a11a8b
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-23 12:04:28 -0500

    Corrige rutas relativas para GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "/miRecetario/index.css";
+
 import AppRoutes from "./routes";
 import { VideoProvider } from "./context/VideosContext";
 
