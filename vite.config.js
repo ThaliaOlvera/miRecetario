add643a9abccdde56b491567acf95b0273e5e315
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-23 13:25:17 -0500

    Fix: deploy2

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Importar el módulo path
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://ThaliaOlvera.gitlab.io/miRecetario",
+  base: "https://ThaliaOlvera.github.io/miRecetario",
 });
