6013c06a8a6d5bda20188072d716c92d59c1aae8
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-01-29 14:42:20 -0500

    Fix:Responsive

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,15 +1,20 @@
 import styles from "./Footer.module.css";
 import Logotipo from "./Logotipo.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
     <footer className={styles.pie}>
-      <h2>Desarrollado por Thalia Olvera un proyecto para Alura Latam</h2>
+      <h2>
+        Desarrollado por Thalia Olvera
+        <br></br>un proyecto para Alura Latam
+      </h2>
 
       <div className={styles.redes}>
-        <h3>Sígueme en mis redes sociales:</h3>
+        <p>Sígueme en mis redes sociales:</p>
         <ul className={styles.listaRedes}>
           <li>
+            <FaLinkedin />
             <a
               href="https://www.linkedin.com/in/thalia-olvera/"
               target="_blank"
@@ -19,6 +24,7 @@ function Footer() {
             </a>
           </li>
           <li>
+            <FaGithub />
             <a
               href="https://github.com/ThaliaOlvera"
               target="_blank"
--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -2,8 +2,26 @@ import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 import Logo from "./Logo.png";
 import CabeceraLink from "../HeaderLink/HeaderLink";
+import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 
 function Header() {
+  const HamburgerMenu = () => {
+    const [menuAbierto, setMenuAbierto] = useState(false);
+
+    const toggleMenu = () => {
+      const nav = document.querySelector("nav");
+      nav.classList.toggle(styles.active);
+      setMenuAbierto(!menuAbierto);
+    };
+
+    return (
+      <button className={styles.hamburger}>
+        <FaBars onClick={toggleMenu} />
+      </button>
+    );
+  };
+
   return (
     <header className={styles.cabecera}>
       <Link to="/">
@@ -11,8 +29,10 @@ function Header() {
           <img src={Logo} alt="Logo Mi Recetario" /> <span>Recetario</span>
         </section>
       </Link>
+      <HamburgerMenu />
+
       <nav>
-        <CabeceraLink url="./">Home</CabeceraLink>
+        <CabeceraLink url="./">Inicio</CabeceraLink>
         <CabeceraLink url="./Favoritos">Favoritos</CabeceraLink>
         <CabeceraLink url="./NuevoVideo">+ Nuevo Video</CabeceraLink>
       </nav>
